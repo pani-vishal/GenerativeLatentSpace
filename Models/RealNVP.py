6a9efbf43cd8e17d10b69574d9249c79e19e3430
Vishal Pani <panivishal17@gmail.com>
2022-03-03 13:21:17 +0100

    [2.03] add method to construct jacobian

--- Models/RealNVP.py
+++ Models/RealNVP.py
@@ -12,6 +12,7 @@ import numpy as np
 from torch import nn
 from torch import distributions
 from torchsummary import summary
+from torch.autograd.functional import jacobian
 
 import sys
 from copy import deepcopy
@@ -22,8 +23,14 @@ sys.path.append('./')
 
 
 class RealNVP(nn.Module):
-    def __init__(self, tfm_layers=6, latent_dim=2, internal_dim=256, device=None):
+    def __init__(self, tfm_layers=6, latent_dim=2, internal_dim=256, distribution="normal", num_modes=1,
+                 masking="checkerboard",device=None):
         super(RealNVP, self).__init__()
+
+        # All the checks here please
+        assert distribution in ["normal", "mixture"], f"Only 'normal' and 'mixture' available, got: '{distribution}'"
+        assert masking in ["checkerboard", "half"], f"Only 'half' and 'checkerboard' available, got: '{masking}'"
+        ###
 
         self.device = device
         if self.device is None:
@@ -31,14 +38,25 @@ class RealNVP(nn.Module):
 
         self.tfm_layers = tfm_layers
         self.latent_dim = latent_dim
+        self.distribution = distribution
+        self.num_modes = num_modes
 
-        # For now, checkerboard masking
         self.mask = np.zeros((tfm_layers, latent_dim), dtype=np.float32)
-        self.mask[1::2, ::2] = 1.
-        self.mask[::2, 1::2] = 1.
+        if masking == "checkerboard":
+            self.mask[1::2, ::2] = 1.
+            self.mask[::2, 1::2] = 1.
+        if masking == "half":
+            self.mask[:, latent_dim//2:] = 1.
         self.mask = nn.Parameter(torch.from_numpy(self.mask), requires_grad=False)
 
         self.prior = distributions.MultivariateNormal(torch.zeros(latent_dim), torch.eye(latent_dim))
+        if self.distribution == "mixture":
+            mean_latent = 5 * torch.cat([torch.eye(num_modes),
+                                         torch.zeros(size=(num_modes, self.latent_dim - num_modes))],
+                                        dim=1)
+            mix = distributions.Categorical(torch.ones(num_modes, ))
+            comp = distributions.Independent(distributions.Normal(mean_latent, torch.ones_like(mean_latent)), 1)
+            self.prior = distributions.MixtureSameFamily(mix, comp)
 
         self.block_scale = nn.Sequential(nn.Linear(latent_dim, internal_dim), nn.LeakyReLU(),
                                          nn.Linear(internal_dim, internal_dim), nn.LeakyReLU(),
@@ -76,16 +94,16 @@ class RealNVP(nn.Module):
             scale_exp = torch.exp(scale)
             trans = self.net_trans[i](x_masked) * (1 - mask)
             x = x_masked + (1 - mask) * (x * scale_exp + trans)
-            print(scale.shape)
             log_det_j += torch.einsum("ijk->i", scale)
 
             if flag_condition:
                 diag_j *= scale_exp
-                eig_max, _ = torch.max(diag_j, axis=-1)
-                eig_min, _ = torch.min(diag_j, axis=-1)
-                cond_num = (eig_max / eig_min).view(-1)
 
         if flag_condition:
+            eig_max, _ = torch.max(diag_j, axis=-1)
+            eig_min, _ = torch.min(diag_j, axis=-1)
+            cond_num = (eig_max / eig_min).view(-1)
+            print(diag_j)
             return x, cond_num
         else:
             return x, log_det_j
@@ -104,8 +122,39 @@ class RealNVP(nn.Module):
         summary(self.block_scale, (64, 1, self.latent_dim))
         summary(self.net_trans, (64, 1, self.latent_dim))
 
+    def get_condition_number_to_image(self, z):
+        jacob = self.get_jacobian_to_image(z)
+        return jacob
 
-def train(dataloader, latent_dim=2, lr=1e-4, max_epochs=1000, tfm_layers=6, device=None):
+    def get_jacobian_to_image(self, z):
+        def func(z):
+            x = z
+            for i in range(len(self.net_scale)):
+                mask = self.mask[i]
+                x_masked = mask * x
+                scale = self.net_scale[i](x_masked) * (1 - mask)
+                scale_exp = torch.exp(scale)
+                trans = self.net_trans[i](x_masked) * (1 - mask)
+                x = x_masked + (1 - mask) * (x * scale_exp + trans)
+            return x
+
+        # TODO: May send this to utils
+        def batch_jacobian(func, x, vectorize=True):
+            """
+            Ref: https://discuss.pytorch.org/t/computing-batch-jacobian-efficiently/80771/4
+            """
+
+            # x in shape (Batch, Length)
+            def _func_sum(x):
+                return func(x).sum(dim=0)
+
+            return jacobian(_func_sum, x, vectorize=vectorize).permute(1, 0, 2)
+
+        return batch_jacobian(func, z)
+
+
+def train(dataloader, latent_dim=2, lr=1e-4, max_epochs=1000, tfm_layers=6, device=None, save_name=None,
+          distribution="normal", masking="checkerboard", num_modes=1):
     if not os.path.exists("Outputs"):
         os.makedirs("Outputs")
     if not os.path.exists("TrainedModels"):
@@ -114,7 +163,8 @@ def train(dataloader, latent_dim=2, lr=1e-4, max_epochs=1000, tfm_layers=6, devi
     if device is None:
         device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    flow = RealNVP(tfm_layers=tfm_layers, latent_dim=latent_dim, device=device).to(device)
+    flow = RealNVP(tfm_layers=tfm_layers, latent_dim=latent_dim, device=device,
+                   distribution=distribution, num_modes=num_modes, masking=masking).to(device)
     flow.train()
 
     # Show the network architectures
@@ -150,10 +200,16 @@ def train(dataloader, latent_dim=2, lr=1e-4, max_epochs=1000, tfm_layers=6, devi
     plt.xlim(0, max_epochs+1)
     plt.legend()
     plt.grid(True)
-    fig.savefig("Outputs/LossHistory.png", bbox_inches='tight')
+    if save_name is None:
+        fig.savefig("Outputs/flow_loss_history.png", bbox_inches='tight')
+    else:
+        fig.savefig(f"Outputs/{save_name}_loss_history.png", bbox_inches='tight')
     plt.close(fig)
 
-    torch.save(flow.state_dict(), "TrainedModels/flow.pth")
+    if save_name is None:
+        torch.save(flow.state_dict(), "TrainedModels/flow.pth")
+    else:
+        torch.save(flow.state_dict(), f"TrainedModels/{save_name}.pth")
 
     return flow
 
